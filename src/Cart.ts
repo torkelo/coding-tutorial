cb611e918cce90d557a1d0336e054b4def52f4b1
Torkel Ödegaard <torkel@grafana.com>
2021-01-22 09:29:58 +0100

    Initial toodo

--- src/Cart.ts
+++ src/Cart.ts
@@ -1,3 +1,15 @@
+
+
+/**
+ * Todo
+ * 1: Add CardItem class with name and price
+ * 2: Add a way to add items to the Cart class and for the Cart class to store them in a class property
+ * 3: Make it possible to get the total price of all items in the Cart
+ * 4: Add a way to print all current cart items in the Cart to the console
+ * 5: Add a function to Cart to sort Cart items by name
+ * 6: Add a function to Cart to add items to the top of the item list so that when you later call the print all items function it is printed first
+ * *
+
 export class Cart {
   public getTotal() {
     return 0;
